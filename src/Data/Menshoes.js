2b6dd989478dae9785568c3782dec5ae5ab4b947
Lam Thanh Phuc <lamthanhphucit@gmail.com>
2023-10-14 19:38:39 +0700

    first commit

---
+++ src/Data/Menshoes.js
@@ -0,0 +1,592 @@
+export const Menshoes =[
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    {
+        "imageUrl":"https://cdn.vuahanghieu.com/unsafe/500x0/left/top/smart/filters:quality(90)/https://admin.vuahanghieu.com/upload/product/2022/06/giay-the-thao-nike-travis-scott-x-air-jordan-1-low-og-reverse-mocha-phoi-mau-62b9388a27724-27062022115642.jpg",
+        "brand" :"Nike",
+        "title":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+        "color":"Mocha",
+        "discountedPrice": 40000000,
+        "price":1000000,
+        "discountPresent" : 25,
+        "size" :[
+            {
+                "name" : "size 7",
+                "quantity":5,
+            },
+            {
+                "name" : "size 8",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 9",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 10",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 11",
+                "quantity":"1 per",
+            },
+            {
+                "name" : "size 12",
+                "quantity":"2 per",
+            },
+            {
+                "name" : "size 13",
+                "quantity":"3 per",
+            },
+            {
+                "name" : "size 14",
+                "quantity":"0 per",
+            }
+        ],
+        "quantity":30,
+        "topLevelCategory":"Men",
+        "secondLevelCategory":"Clothing",
+        "thirdLevelCategory":"Men_Shoes",
+        "description":" Nike Travis Scott X Air Jordan 1 Low OG Reverse Mocha",
+
+    },
+    
+    
+]
---
+++ src/components/Footer/Footer..jsx
@@ -0,0 +1,82 @@
+import { Button, Grid, Link, Typography } from '@mui/material'
+import React from 'react'
+
+const Footer = () => {
+    return (
+        <div>
+            <Grid className='bg-black text-white text-center mt-10' container sx={{ bgcolor: "black", color: "white", py: 3 }}>
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className='pb-5 text-sm ' variant='h6' gutterBottom >Company</Typography>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >About</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Blog</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Press</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Jobs</Button>
+                    </div>
+                </Grid>
+                
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className='pb-5 text-sm ' variant='h6' gutterBottom >Company</Typography>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >About</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Blog</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Press</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Jobs</Button>
+                    </div>
+                </Grid>
+
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className='pb-5 text-sm ' variant='h6' gutterBottom >Company</Typography>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >About</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Blog</Button>
+                    </div>
+                </Grid>
+
+                <Grid item xs={12} sm={6} md={3}>
+                    <Typography className='pb-5 text-sm ' variant='h6' gutterBottom >Company</Typography>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >About</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Blog</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Press</Button>
+                    </div>
+                    <div>
+                        <Button className='pb-5 text-sm ' variant='h6' gutterBottom >Jobs</Button>
+                    </div>
+                </Grid>
+
+                <Grid className='pt-20 ' sm={12}>
+                    <Typography variant='body2' component="p" align='center'>
+                        &copy; 2023 My Company
+                    </Typography>
+                    <Typography variant='body2' component="p" align='center'>
+                        &copy; Make With Love By Me 
+                    </Typography>
+                    <Link href="" color="inherit" underline='alway'>
+                    </Link>
+                </Grid>
+            </Grid>
+            
+        </div>
+    )
+}
+
+export default Footer
